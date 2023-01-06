3989f9519347b81bcbb50f6da0b29b288fac0985
minalk9 <109914400+minalk9@users.noreply.github.com>
2023-01-06 16:27:50 +0530

    Add files via upload

--- microsoft/Course Schedule.cpp
+++ microsoft/Course Schedule.cpp
@@ -1,38 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-       vector<int>adj[V];
-    int edges = prerequisites.size();
-    
-    for(int i = 0;i<edges;i++){
-            int u = prerequisites[i][0];
-            int v = prerequisites[i][1];
-            adj[v].push_back(u);
-     }
-        
-  
-        vector<int> indegree(V, 0);
-        for(int i =0;i<V;i++){
-            for(auto it:adj[i]) {
-                indegree[it]++;
-            }
+        vector<vector<int>> adj(numCourses, vector<int>());
+        vector<int> indegrees(numCourses,0);
+        for(auto val: prerequisites){
+            indegrees[val[0]]++; adj[val[1]].emplace_back(val[0]);
         }
-        queue<int>q;
-        for(int i=0;i<V;i++){
-            if(indegree[i]==0) q.push(i);
+        queue<int> q; int processed=0;
+        for(int i=0;i<numCourses; i++){
+            if(indegrees[i]==0) q.push(i);
         }
-        vector<int>topo;
         while(!q.empty()){
-            int node = q.front();
-            q.pop();
-            topo.push_back(node);
-            
-            for(auto it:adj[node]){
-                indegree[it]--;
-                if(indegree[it] ==0) q.push(it);
+            int curr= q.front(); q.pop(); processed++;
+            for(int i=0; i<adj[curr].size(); i++){
+                if( --indegrees[adj[curr][i]]==0 ) q.push(adj[curr][i]);
             }
         }
-        if(topo.size()==V) return true;
-        return false; 
+        return processed==numCourses;
     }
 };
