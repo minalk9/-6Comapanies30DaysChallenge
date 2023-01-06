79818f3cb0e8b337e4ee425ddfac8fdae3513587
minalk9 <109914400+minalk9@users.noreply.github.com>
2023-01-06 16:35:12 +0530

    Add files via upload

--- microsoft/Number of Ways to Arrive at Destination.cpp
+++ microsoft/Number of Ways to Arrive at Destination.cpp
@@ -1,35 +1,40 @@
+#define ll long long
 class Solution {
 public:
     int MOD = 1e9 + 7;
     int countPaths(int n, vector<vector<int>>& roads) {
-        vector<vector<pll>> graph(n);
-        for(auto& road: roads) {
-            ll u = road[0], v = road[1], time = road[2];
-            graph[u].push_back({v, time});
-            graph[v].push_back({u, time});
-        }
-        return dijkstra(graph, n, 0);
-    }
-    int dijkstra(const vector<vector<pll>>& graph, int n, int src) {
-        vector<ll> dist(n, LONG_MAX);
-        vector<ll> ways(n);
-        ways[src] = 1;
-        dist[src] = 0;
-        priority_queue<pll, vector<pll>, greater<>> minHeap;
-        minHeap.push({0, 0}); // dist, src
-        while (!minHeap.empty()) {
-            auto[d, u] = minHeap.top(); minHeap.pop();
-            if (d > dist[u]) continue; // Skip if `d` is not updated to latest version!
-            for(auto [v, time] : graph[u]) {
-                if (dist[v] > d + time) {
-                    dist[v] = d + time;
-                    ways[v] = ways[u];
-                    minHeap.push({dist[v], v});
-                } else if (dist[v] == d + time) {
-                    ways[v] = (ways[v] + ways[u]) % MOD;
-                }
-            }
-        }
-        return ways[n-1];
+        vector<pair<ll,ll>> adj[n];
+       for(auto it:roads){
+           adj[it[0]].push_back({it[1],it[2]});
+           adj[it[1]].push_back({it[0],it[2]});
+       }
+       
+       priority_queue<pair<ll,ll>,vector<pair<ll,ll>>,greater<pair<ll,ll>>> pq;
+       vector<ll> dist(n,LONG_MAX), ways(n,0);
+       dist[0] = 0;
+       ways[0] = 1;
+       
+       pq.push({0,0});
+       
+       while(!pq.empty()){
+           ll dis = pq.top().first;
+           ll node = pq.top().second;
+           pq.pop();
+           
+           for(auto it: adj[node]){
+               ll adjNode = it.first;
+               ll edW = it.second;
+               
+               if(dis + edW < dist[adjNode]){
+                   dist[adjNode] = dis + edW;
+                   pq.push({dis + edW,adjNode});
+                   ways[adjNode] = ways[node];
+               }
+               else if(dis +edW == dist[adjNode]){
+                   ways[adjNode] = (ways[adjNode] + ways[node])%MOD;
+               }
+           }
+       }
+       return ways[n-1];
     }
 };
